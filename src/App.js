b7b835c19d926ecd80d2e5c6ff5a12a6c67ae7ef
Trevor Mackin <tismetrm@gmail.com>
2020-11-21 14:08:38 -0800

    Wrap component in react-router Switch statements and add Home component with path

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment } from 'react';
+import { Switch, Route, withRouter } from 'react-router-dom';
+import Home from './Views/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+function App(props) {
+
+    return (
+        <Fragment>
+            <Switch>
+                <Route exact path='/' component={Home} />
+            </Switch>
+        </Fragment>
+    );
 }
 
-export default App;
+export default withRouter(App);
