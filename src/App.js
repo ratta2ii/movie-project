488cbe466556521bff87d3b00fe68fe01a032a46
Trevor Mackin <tismetrm@gmail.com>
2020-11-23 23:50:43 -0800

    Add new routes, one for the list of movies and one for the movie info view

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Fragment } from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 import Home from './Views/Home/Home';
-
+import Movies from './Views/Movies/Movies';
+import MovieInfo from './Views/MovieInfo/MovieInfo';
 
 
 function App(props) {
-
     return (
         <Fragment>
             <Switch>
                 <Route exact path='/' component={Home} />
+                <Route path='/movies' component={Movies} />
+                <Route path='/movie-info' component={MovieInfo} />
             </Switch>
         </Fragment>
     );
 }
+
 
 export default withRouter(App);
