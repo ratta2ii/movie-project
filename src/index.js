04b63642d2d450ae3dea6588e8f59ad74dc21c46
Trevor Mackin <tismetrm@gmail.com>
2020-11-25 15:42:19 -0800

    Add the following: Hashrouter, Redux provider, rootreducer, thunkMiddleware, middlewareLogger, and use these to create a store

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css';
+import rootReducer from './Reducers/index';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import middlewareLogger from './Middleware/middleware-logger';
+
+
+export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, middlewareLogger));
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Provider store={store}>
+            <HashRouter>
+                <App />
+            </HashRouter>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
