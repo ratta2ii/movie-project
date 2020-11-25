2d75247230500645177f76b8cbea455cecd2c918
Trevor Mackin <tismetrm@gmail.com>
2020-11-25 14:10:51 -0800

    Add TopRibbon component that is used fot aesthetics

---
+++ src/Components/TopRibbon/TopRibbon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import useStyles from './TopRibbonStyles';
+
+
+const TopRibbon = (props) => {
+
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root} ></Box>
+    )
+};
+
+export default TopRibbon;
---
+++ src/Components/TopRibbon/TopRibbonStyles.js
@@ -0,0 +1,23 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+export const useStyles = makeStyles((theme) => ({
+    root: {
+        width: '100vw',
+        height: '20vh',
+        backgroundColor: '#f4433699',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        borderBottom: '5px solid red',
+    },
+    homeIcon: {
+        color: '#ffffff',
+        fontSize: 100,
+        position: 'fixed',
+        top: 50,
+        right: 50,
+    }
+}));
+
+export default useStyles;
