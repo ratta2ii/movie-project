975003d9233b17bda882a57d118444796b69c4f3
Trevor Mackin <tismetrm@gmail.com>
2020-11-25 14:11:57 -0800

    Add an Error404 component that will render errors to the UI

--- src/App.js
+++ src/App.js
@@ -3,19 +3,25 @@ import { Switch, Route, withRouter } from 'react-router-dom';
 import Home from './Views/Home/Home';
 import Movies from './Views/Movies/Movies';
 import MovieInfo from './Views/MovieInfo/MovieInfo';
+import TopRibbon from './Components/TopRibbon/TopRibbon';
+import Navigation from './Components/Navigation/Navigation';
+import Error404 from './Views/Error404/Error404';
 
 
-function App(props) {
+const App = () => {
+
     return (
         <Fragment>
+            <TopRibbon />
+            <Navigation />
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route path='/movies' component={Movies} />
                 <Route path='/movie-info' component={MovieInfo} />
+                <Route component={Error404} />
             </Switch>
         </Fragment>
     );
 }
-
 
 export default withRouter(App);
---
+++ src/Views/Error404/Error404.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import useStyles from './Error404Styles';
+
+
+function Error404(props) {
+
+    const classes = useStyles();
+
+    return (
+        <Box className={classes.root}>
+            <Typography variant='h4' 
+                className={classes.errorText}>
+                The page {props.location.pathname} does not exist!
+            </Typography>
+            <Typography variant='h6'>
+                Would you like to return
+                <Link to="/" className={classes.linkText} >
+                    HOME
+                </Link> 
+                instead?
+            </Typography>
+        </Box>
+    );
+}
+
+Error404.propTypes = {
+    location: PropTypes.object,
+};
+
+export default Error404;
