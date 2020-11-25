b8097b14990bc95b331d07203b6003e4d6cac87d
Trevor Mackin <tismetrm@gmail.com>
2020-11-25 15:56:25 -0800

    Add functionality to ensure a user is only allowed one vote per session Add dynamic cursor properties, and color properties to indicate a users voting status on whether or not they previously voted Update comments throughout MovieInfo component with details on some of the functionality

--- src/Views/MovieInfo/MovieInfo.js
+++ src/Views/MovieInfo/MovieInfo.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
+import Link from 'react-dom';
 import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import ThumbDownIcon from '@material-ui/icons/ThumbDown';
@@ -9,28 +10,30 @@ import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import Footer from './../../Components/Footer/Footer';
 import useStyles from './MovieInfoStyles';
 import { addVoteToDb } from './../../FirebaseDB/request';
 
 
 const MovieInfo = (props) => {
 
-
     let defaultVotes = {
         upVotes: 0,
         downVotes: 0,
-    }
+    };
 
     const { movieInfo, movieInfoError, isLoading } = props;
     const { Poster, Title, Director, Released, Plot } = movieInfo;
     const [currentVotes, setCurrentVotes] = useState(defaultVotes);
+    // One vote per user, per session (since no auth), however, you can undo and vote again.
+    const [upVoteStatus, setUpVoteStatus] = useState(false);
+    const [downVoteStatus, setDownVoteStatus] = useState(false);
     const classes = useStyles();
-
 
     useEffect(() => {
         //* This checks to see if the current movie selected has previous votes in the
-        //* in the database by using the imdbID. If it finds an imdbID match, it retrieves
-        //* the vote totals and renders them in place of the default votes (0, 0).
+        //* database by using the imdbID. If it finds an imdbID match, it retrieves the
+        //* vote totals and renders them instead of the default vote totals (0, 0).
         //? Move to a different module later?
         if (movieInfo.imdbID) {
             var moviesRef = db.collection("movies");
@@ -45,78 +48,95 @@ const MovieInfo = (props) => {
                     }
                     return false;
                 })
-            })
-            .catch(error => {
+            }).catch(error => {
                 console.log(error.message);
-            })
+            });
         }
     }, [movieInfo.imdbID]);
-
 
     useEffect(() => {
         return () => console.log('unmounting...');
     }, []);
 
-
     //* add(Up/Down)Vote updates the database as well as the local state for the vote totals.
     //* The local state will always show a users votes while also updating the db to any changes.
-    //* When the component first mounts, the database returns updated vote counts (see in useEffect)
-    const addUpVote = () => {
+    //* When the component first mounts, the database returns current vote counts -see useEffect().
+    //* Note: One vote per user, per session (since no auth), however, you can undo and vote again.
+    const handleUpVote = () => {
+        if (downVoteStatus) return false;
         let voteUpdate = {
-            upVotes: currentVotes.upVotes + 1,
+            upVotes: currentVotes.upVotes,
             downVotes: currentVotes.downVotes,
             imdbID: movieInfo.imdbID,
             title: Title
         };
-        addVoteToDb(voteUpdate);
-        setCurrentVotes({
-            ...currentVotes,
-            upVotes: currentVotes.upVotes + 1
-        });
-    }
+        if (!upVoteStatus) {
+            voteUpdate.upVotes = currentVotes.upVotes + 1;
+            addVoteToDb(voteUpdate);
+            setUpVoteStatus(true);
+            setCurrentVotes({
+                ...currentVotes,
+                upVotes: currentVotes.upVotes + 1
+            });
+        } else {
+            voteUpdate.upVotes = currentVotes.upVotes - 1;
+            addVoteToDb(voteUpdate);
+            setUpVoteStatus(false);
+            setCurrentVotes({
+                ...currentVotes,
+                upVotes: currentVotes.upVotes - 1
+            });
+        }
+    };
 
-
-    const addDownVote = () => {
+    //* Note: One vote per user, per session (since no auth), however, you can undo and vote again.
+    const handleDownVote = () => {
+        if (upVoteStatus) return false;
         let voteUpdate = {
             upVotes: currentVotes.upVotes,
             downVotes: currentVotes.downVotes + 1,
             imdbID: movieInfo.imdbID,
             title: Title
         }
-        addVoteToDb(voteUpdate);
-        setCurrentVotes({
-            ...currentVotes,
-            downVotes: currentVotes.downVotes + 1
-        });
-    }
-
+        if (!downVoteStatus) {
+            voteUpdate.downVotes = currentVotes.downVotes + 1;
+            addVoteToDb(voteUpdate);
+            setDownVoteStatus(true);
+            setCurrentVotes({
+                ...currentVotes,
+                downVotes: currentVotes.downVotes + 1
+            });
+        } else {
+            voteUpdate.downVotes = currentVotes.downVotes - 1;
+            addVoteToDb(voteUpdate);
+            setDownVoteStatus(false);
+            setCurrentVotes({
+                ...currentVotes,
+                downVotes: currentVotes.downVotes - 1
+            });
+        }
+    };
 
     const handleBackToMovies = () => {
         props.history.push('/movies');
-    }
+    };
 
-
-    if (movieInfoError) {
-        return <React.Fragment>Error: {movieInfoError.message}</React.Fragment>;
-    }
-    else if (isLoading) {
-        return <Box>...still loading</Box>
-    }
-    else {
+    if (movieInfo) {
         return (
             <Box className={classes.root} >
                 <Paper className={classes.paperCon} >
                     <Grid container>
                         {/* Main movie poster grid container */}
-                        <Grid item xs={12} md={5} className={classes.posterImgGridItem} >
+                        <Grid item xs={12} md={6} className={classes.posterImgGridItem} >
                             <Box className={classes.posterImgCon} >
                                 <img src={Poster}
                                     alt="movie poster"
-                                    className={classes.posterImg} />
+                                    className={classes.posterImg}
+                                />
                             </Box>
                         </Grid>
                         {/* Main movie info container */}
-                        <Grid item xs={12} md={7} >
+                        <Grid item xs={12} md={6} >
                             <Grid container>
                                 <Box className={classes.infoBox} >
                                     {/* Movie info top container */}
@@ -125,10 +145,10 @@ const MovieInfo = (props) => {
                                             {Title}
                                         </Typography>
                                         <Typography className={classes.dirNdate} >
-                                            {Released}
+                                            Release Date ({Released})
                                         </Typography>
                                         <Typography className={classes.dirNdate} >
-                                            Director: {Director}
+                                            Director by {Director}
                                         </Typography>
                                     </Grid>
                                     {/* Movie info bottom container */}
@@ -142,34 +162,46 @@ const MovieInfo = (props) => {
                         </Grid>
                         {/* Bottom of main container */}
                         <Grid container className={classes.actionAreaGridCon} >
-                            <Grid item md={5} >
+                            <Grid item md={6} >
                                 <Box
                                     className={classes.backContainer}
                                     onClick={handleBackToMovies} >
-                                    <ArrowBackIosIcon />
+                                    <ArrowBackIosIcon className={classes.arrowIcon} />
                                     <Button className={classes.backButton} >
                                         Back to Movies
                                     </Button>
                                 </Box>
                             </Grid>
-                            <Grid item md={7}>
+                            <Grid item md={6}>
                                 <Box className={classes.votesBox} >
                                     <Typography variant="h6">
-                                        <Typography 
+                                        <Typography
                                             className={classes.voteText}
                                             variant="title">
-                                            DOWN VOTES  [ {currentVotes.downVotes} ] 
-                                    </Typography>
-                                        <ThumbDownIcon 
+                                            DOWN VOTES  [ {currentVotes.downVotes} ]
+                                        </Typography>
+                                        <ThumbDownIcon
                                             className={classes.downThumbIcon}
-                                            onClick={() => addDownVote()} />
-                                        <ThumbUpIcon 
+                                            onClick={() => handleDownVote()}
+                                            style={{
+                                                color: (downVoteStatus) ? '#89b9df' : '#637c90',
+                                                cursor: (downVoteStatus ||
+                                                    ((!downVoteStatus) && (!upVoteStatus))) ? 'pointer' : 'no-drop'
+                                            }}
+                                        />
+                                        <ThumbUpIcon
                                             className={classes.upThumbIcon}
-                                            onClick={() => addUpVote()} />
-                                        <Typography 
+                                            onClick={() => handleUpVote()}
+                                            style={{
+                                                color: (upVoteStatus) ? '#89b9df' : '#637c90',
+                                                cursor: (upVoteStatus ||
+                                                    ((!upVoteStatus) && (!downVoteStatus))) ? 'pointer' : 'no-drop'
+                                            }}
+                                        />
+                                        <Typography
                                             className={classes.voteText}
                                             variant="title">
-                                                [ {currentVotes.upVotes} ] UP VOTES
+                                            [ {currentVotes.upVotes} ] UP VOTES
                                         </Typography>
                                     </Typography>
                                 </Box>
@@ -177,12 +209,28 @@ const MovieInfo = (props) => {
                         </Grid>
                     </Grid>
                 </Paper>
-
+                <Footer />
             </Box>
         );
     }
-}
-
+    // ! Currently no styling for errors (Build error component to reuse when necessary)
+    else if (movieInfoError) {
+        return <React.Fragment>Error: {movieInfoError.message}</React.Fragment>;
+    }
+    else if (isLoading) {
+        return <Box>...still loading</Box>
+    }
+    else {
+        return (
+            <Box>
+                <Typography variant='h4'>
+                    There seems to be a problem.
+                </Typography>
+                <Link to='/'>Return Home</Link>
+            </Box>
+        );
+    }
+};
 
 const mapStateToProps = state => {
     return {
@@ -190,8 +238,7 @@ const mapStateToProps = state => {
         movieInfoError: state.movieInfoSlice.error,
         isLoading: state.movieInfoSlice.isLoading,
     }
-}
-
+};
 
 export default connect(mapStateToProps)(MovieInfo);
 
