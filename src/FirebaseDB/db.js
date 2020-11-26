31afb110ffd256b50066722fed169284c46b8ca5
Trevor Mackin <tismetrm@gmail.com>
2020-11-25 22:15:38 -0800

    Update ES Modules to only import packages relevant to the app

--- src/FirebaseDB/db.js
+++ src/FirebaseDB/db.js
@@ -1,6 +1,6 @@
 import firebaseConfig from "./config";
-const firebase = require("firebase");
-require("firebase/firestore");
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 
 // Initialize Firebase DB if none exist under current configs
 // This statement is necessary to prevent errors
